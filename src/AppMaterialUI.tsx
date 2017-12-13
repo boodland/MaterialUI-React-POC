4a305af61c970bf0c3595fa1b5c37a13ac031077
Alberto Arias <alberto@boodland.com>
2017-12-13 19:57:54 +0100

    Add Drawer to AppMaterialUI module

--- src/AppMaterialUI.tsx
+++ src/AppMaterialUI.tsx
@@ -3,5 +3,6 @@ import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import Drawer from 'material-ui/Drawer';
 
-export { AppBar, Toolbar, Typography, IconButton, MenuIcon };
+export { AppBar, Toolbar, Typography, IconButton, MenuIcon, Drawer };
