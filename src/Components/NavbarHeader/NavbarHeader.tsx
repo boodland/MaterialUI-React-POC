0ace7945b51107bf19d7203f1ba183fddd53a477
Alberto Arias <alberto@boodland.com>
2017-12-13 19:09:58 +0100

    Create AppBar within NavbarHeader component

--- src/Components/NavbarHeader/NavbarHeader.tsx
+++ src/Components/NavbarHeader/NavbarHeader.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { AppBar, Toolbar, Typography } from '../../AppMaterialUI';
+
+import './NavbarHeader.css';
 
 export interface Props {
   title: string;
 }
 
 const NavbarHeader = ({ title }: Props ) => (
-  <p>Navbar Header Component with title {title} Works!</p>
+  <div>
+    <AppBar position="static">
+        <Toolbar className="navbar-header-toolbar">
+          <Typography type="title" color="inherit">
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  </div>
 );
 
 export default NavbarHeader;
