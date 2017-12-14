c76c5951b781a6adb7f9ea58b8a6658e02196776
Alberto Arias <alberto@boodland.com>
2017-12-14 14:00:10 +0100

    Add new NavbarActions component to the App Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import './App.css';
 
 import NavbarHeaderContainer from './Components/NavbarHeader/NavbarHeaderContainer';
+import NavbarActionsContainer from './Components/NavbarActions/NavbarActionsContainer';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <NavbarHeaderContainer />
+        <NavbarActionsContainer />
       </div>
     );
   }
