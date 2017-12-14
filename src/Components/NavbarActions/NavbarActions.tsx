41aa993ed2ee695eb79fa7f0f36f7dfff669b1fd
Alberto Arias <alberto@boodland.com>
2017-12-14 15:55:03 +0100

    Extract actions elements within listActions var

--- src/Components/NavbarActions/NavbarActions.tsx
+++ src/Components/NavbarActions/NavbarActions.tsx
@@ -7,27 +7,29 @@ export interface Props {
   close: Function;
 }
 
-const NavbarActions = ({ actions, open, close }: Props) => (
-  <Drawer open={open} onRequestClose={() => close()}>
-    <div
-      tabIndex={0}
-      role="button"
-      onClick={() => close()}
-      onKeyDown={() => close()}
-    >
-      <List>
-        {actions.map((action, id) => (
-          <ListItem key={id} button={true}>
-            <ListItemIcon>
-              <Icon>{action.icon}</Icon>
-            </ListItemIcon>
-            <ListItemText primary={action.title} />
-          </ListItem>
-          )
-        )}
-      </List>
-    </div>
-  </Drawer>
-);
+const NavbarActions = ({ actions, open, close }: Props) => {
+  const listActions = actions.map((action, id) => (
+    <ListItem key={id} button={true}>
+      <ListItemIcon>
+        <Icon>{action.icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={action.title} />
+    </ListItem>
+    )
+  );
+  return (
+    <Drawer open={open} onRequestClose={() => close()}>
+      <div
+        role="button"
+        onClick={() => close()}
+        onKeyDown={() => close()}
+      >
+        <List>
+          {listActions}
+        </List>
+      </div>
+    </Drawer>
+  );
+};
 
 export default NavbarActions;
