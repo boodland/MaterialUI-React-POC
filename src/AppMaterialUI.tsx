cb2a0512f9fbf77ded5c6e7e6ee8ca79ac93c6d6
Alberto Arias <alberto@boodland.com>
2017-12-14 13:58:10 +0100

    Add more materials to AppMaterial module

--- src/AppMaterialUI.tsx
+++ src/AppMaterialUI.tsx
@@ -4,5 +4,16 @@ import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Icon from 'material-ui/Icon';
 
-export { AppBar, Toolbar, Typography, IconButton, MenuIcon, Drawer };
+export {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  MenuIcon,
+  Drawer,
+  List, ListItem, ListItemIcon, ListItemText,
+  Icon
+};
