e54756e251f80608c3be6a3f422959b63a3e4d1f
Alberto Arias <alberto@boodland.com>
2017-12-15 13:00:53 +0100

    Create brand new Redux store with the new reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import navbarReducer from './Components/Navbar/NavbarReducer';
+
 import App from './App';
 import './index.css';
 
+let store = createStore(navbarReducer);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
