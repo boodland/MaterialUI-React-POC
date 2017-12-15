08139c74f8fa852eafe32d472e92d15fc440b7cd
Alberto Arias <alberto@boodland.com>
2017-12-15 13:03:18 +0100

    Make NavbarContainer a pure function as the state is managed using redux

--- src/Components/Navbar/NavbarContainer.tsx
+++ src/Components/Navbar/NavbarContainer.tsx
@@ -1,39 +1,13 @@
 import * as React from 'react';
 
-import NavbarHeaderContainer, {Props as NavbarHeaderProps } from '../NavbarHeader/NavbarHeaderContainer';
-import NavbarActionsContainer , { Props as NavbarActionsProps } from '../NavbarActions/NavbarActionsContainer';
+import NavbarHeaderConnected from '../NavbarHeader/NavbarHeaderConnected';
+import NavbarActionsConnected  from '../NavbarActions/NavbarActionsConnected';
 
-interface State {
-  open: boolean;
-}
-
-class NavbarContainer extends React.Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { open: false };
-  }
-
-  toggleNavBar = (state: boolean) => {
-    this.setState({ open: state });
-  }
-
-  render() {
-    const navbarActionsProps: NavbarActionsProps = {
-        open: this.state.open,
-        onNavbarClose: () => this.toggleNavBar(false)
-    };
-
-    const navbarHeaderProps: NavbarHeaderProps = {
-      onNavbarOpen: () => this.toggleNavBar(true)
-    };
-
-    return (
-      <React.Fragment>
-        <NavbarHeaderContainer {...navbarHeaderProps}/>
-        <NavbarActionsContainer {...navbarActionsProps}/>
-      </React.Fragment>
-    );
-  }
-}
+const NavbarContainer = () => (
+  <React.Fragment>
+    <NavbarHeaderConnected />
+    <NavbarActionsConnected />
+  </React.Fragment>
+);
 
 export default NavbarContainer;
