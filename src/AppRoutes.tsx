1fde597e92202e4dffefa089f48aaae522ca7292
Alberto Arias <alberto@boodland.com>
2017-12-20 17:38:03 +0100

    Refactor AppRoutes to load SubjectsRoutes rather than Subjects component

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,13 +3,13 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import DashboardListContainer from './Components/DashboardList/DashboardListContainer';
 
-import { LoadableSubjects } from './AppLoader';
+import SubjectsRoutes from './Components/Subjects/SubjectsRoutes';
 
 const Routes = () => (
   <Switch>
     <Redirect path="/" exact={true} to="/dashboard"/>
     <Route path="/dashboard" component={DashboardListContainer} />
-    <Route path="/subjects" component={LoadableSubjects} />
+    <Route path="/subjects" component={SubjectsRoutes} />
     <Redirect to="/dashboard"/>
   </Switch>
 );
