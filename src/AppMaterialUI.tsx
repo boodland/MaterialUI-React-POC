644dae3b9695df4a8bebcd7eca82867d79bffb7e
Alberto Arias <alberto@boodland.com>
2017-12-20 20:19:29 +0100

    First version subject details component

--- src/AppMaterialUI.tsx
+++ src/AppMaterialUI.tsx
@@ -14,6 +14,7 @@ import TextField from 'material-ui/TextField';
 import MenuItem from 'material-ui/Menu/MenuItem';
 import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
 import { FormControl } from 'material-ui/Form';
+import ExpansionPanel, { ExpansionPanelDetails, ExpansionPanelSummary } from 'material-ui/ExpansionPanel';
 
 export {
   AppBar,
@@ -31,5 +32,6 @@ export {
   TextField,
   MenuItem,
   Input, InputLabel, InputAdornment,
-  FormControl
+  FormControl,
+  ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary
 };
--- src/Components/SubjectDetails/SubjectDetails.tsx
+++ src/Components/SubjectDetails/SubjectDetails.tsx
@@ -1,12 +1,73 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
 
+import {
+  ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails, Icon
+} from '../../AppMaterialUI';
+
+import './SubjectDetails.css';
+
 type SubjectDetailsProp = {
   subjectId: string;
 };
 
 const SubjectDetails = ({ match }: RouteComponentProps<SubjectDetailsProp>) => (
-  <p>SubjectDetails Works! {match.params.subjectId}</p>
+  <div className="subject-details-panel">
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<Icon>info_outline</Icon>}>
+        <Typography>General settings</Typography>
+        <Typography>I am an expansion panel</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+          maximus est, id dignissim quam.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<Icon>event</Icon>}>
+        <Typography>Users</Typography>
+        <Typography>
+          You are currently not an owner
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+          diam eros in elit. Pellentesque convallis laoreet laoreet.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<Icon>report_problem</Icon>}>
+        <Typography>Advanced settings</Typography>
+        <Typography>
+          Filtering has been entirely disabled for whole web server
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+          eros, vitae egestas augue. Duis vel est augue.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<Icon>library_books</Icon>}>
+        <Typography>Advanced settings</Typography>
+        <Typography>
+          Filtering has been entirely disabled for whole web server
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+          eros, vitae egestas augue. Duis vel est augue.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  </div>
 );
 
 export default withRouter(SubjectDetails);
