84fcc6d9dc38f809a5fcf08fba10565058c1d8e3
Alberto Arias <alberto@boodland.com>
2018-01-23 14:03:11 +0100

    Some refactoring

--- src/Components/Tutors/TutorsRoutes.tsx
+++ src/Components/Tutors/TutorsRoutes.tsx
@@ -4,7 +4,6 @@ import { Route, Switch, RouteComponentProps,  } from 'react-router-dom';
 import { AppLoadable } from '../../AppLoader';
 
 const TutorsImport = () => import('../TutorList/TutorListContainer');
-
 const TutorListLoadable = AppLoadable(TutorsImport);
 
 const TutorsRoutes = ({router, path}: {router: RouteComponentProps<{location: Location}> , path: string}) => (
@@ -12,10 +11,7 @@ const TutorsRoutes = ({router, path}: {router: RouteComponentProps<{location: Lo
     <Route
       path={path}
       exact={true}
-      render={
-        props =>
-        <TutorListLoadable />
-      }
+      render={() => <TutorListLoadable />}
     />
   </Switch>
 );
